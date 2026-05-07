3c76d534dd1e9eaa157bef27b4734c1d85c956e7
gituser <danjee2009@gmail.com>
2026-05-07 21:33:26 +0900

    feat: common_response 생성

---
+++ app/core/common_response.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Generic, TypeVar, Optional
+
+T = TypeVar("T")
+
+class CommonResponse(BaseModel, Generic[T]):
+    status: str = "ok"
+    message: Optional[str] = None
+    data: Optional[T] = None
+
