72a52ac82351dcc1d9f8c151449933f99ca3aa4e
kimwuju <ugim60350@gmail.com>
2026-05-18 20:11:34 +0900

    feat: 깃허브 파일 구조, readme, package.json 추출

---
+++ app/core/filters.py
@@ -0,0 +1,33 @@
+BLACK_DIRS = {
+    ".git",
+    "node_modules",
+    "__pycache__",
+    ".idea",
+    ".next",
+    "dist",
+    "build",
+    "coverage"
+}
+
+BLACK_EXTENSIONS = {
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".svg",
+    ".mp4",
+    ".zip",
+    ".lock"
+}
+
+WHITE_EXTENSIONS = {
+    ".py",
+    ".js",
+    ".ts",
+    ".jsx",
+    ".tsx",
+    ".java",
+    ".kt",
+    ".go",
+    ".rs"
+}
